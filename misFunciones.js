ecb2bd6fa6c14c2358663fc425c9014c7a950653
MariaRuival <2207446@ucc.edu.ar>
2023-06-02 17:03:03 -0300

    Se agregaron funciones de Js

--- misFunciones.js
+++ misFunciones.js
@@ -5,3 +5,24 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cargarWeb(){
+    var num1, num2, incog, urlComp;
+    incog = document.getElementById("calcular").value;
+    if(incog=="distancia"){
+        incog = num1 * num2;
+    }else if(incog=="tiempo"){
+        incog = num1 / num2;
+    }else if(incog=="velocidad"){
+        incog = num1 / num2;
+    }
+    urlComp = "en_movimiento_2.html #" + incog + "#";
+    window.open(urlComp);
+}
+
+function cargarResultado(){
+    var urlComp, inc;
+    urlComp = window.location.href.split("/").[5];
+    inc = urlComp.split("/").[1];
+
+    document.getElementById("resul").value = inc;
+}
